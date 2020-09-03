97b0415325783a27f0b9c67d4f24fdb1efaba5b2
Minh Nguyen <tmnguyen1403@gmail.com>
2020-09-03 01:02:33 -0500

    09/03/2020 Store data in 10minutes if app restart

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -14,14 +14,40 @@ class ViewController: UIViewController {
   @IBOutlet weak var tipLabel: UILabel!
   @IBOutlet weak var totalLabel: UILabel!
   @IBOutlet weak var tipControl: UISegmentedControl!
+  let TIME_DATA = 10 * 60.0//data is stored in 10 mins
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     
     //Toggle keyboard for billField when the app is launched
     self.billField.becomeFirstResponder()
+    print("view DidLoad")
+    //if app restart, load the data from previous calculation
+    loadData()
   }
 
+  func loadData() {
+    let defaults = UserDefaults.init()
+    let savedStartTime = defaults.double(forKey: "start_time")
+    let timePassed = NSDate().timeIntervalSince1970 - savedStartTime
+    //if over time, do not load data
+    if timePassed > TIME_DATA {
+      return
+    }
+    //load data
+    let savedBill = defaults.float(forKey: "bill")
+    let savedTip = defaults.float(forKey: "tip")
+    let savedTotal = defaults.float(forKey: "total")
+    let savedTipPercent = defaults.integer(forKey: "tip_percent")
+    if savedBill != 0 {
+      billField.text = String(format: "%.2f", savedBill)
+      tipLabel.text = String(format: "$%.2f", savedTip)
+      totalLabel.text = String(format: "$%.2f", savedTotal)
+      tipControl.selectedSegmentIndex = savedTipPercent
+      print("time passed: ", NSDate().timeIntervalSince1970 - savedStartTime)
+    }
+  }
+  
   @IBAction func onTap(_ sender: Any) {
     print("hello")
     
@@ -39,6 +65,14 @@ class ViewController: UIViewController {
     //update the tip and total labels
     tipLabel.text = String(format: "$%.2f", tip)
     totalLabel.text = String(format: "$%.2f", total)
+    
+    //Save data to load when app restart
+    let defaults = UserDefaults.init()
+    defaults.set(tip, forKey: "tip")
+    defaults.set(bill, forKey: "bill")
+    defaults.set(total, forKey: "total")
+    defaults.set(tipControl.selectedSegmentIndex, forKey: "tip_percent")
+    defaults.set(NSDate().timeIntervalSince1970, forKey: "start_time")
   }
 }
 
