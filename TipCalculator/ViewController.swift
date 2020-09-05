e713153b2fb3a16b17d8615dc85e950dd57ed8cd
Minh Nguyen <tmnguyen1403@gmail.com>
2020-09-05 11:43:44 -0500

    2020-09-05 Change currency, separator upon regions
    1. Currency symbol, thousand separator, decimal separator are changed
    based on user's Region Setting
    2. When decimal number use comma instead of dot, the comma need to be
    replaced by dot in order to convert that number into string successfully

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
   @IBOutlet weak var totalLabel: UILabel!
   @IBOutlet weak var tipControl: UISegmentedControl!
   let TIME_DATA = 10 * 60.0//data is stored in 10 mins
+  var currencySymbol : String = ""
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
@@ -22,10 +23,35 @@ class ViewController: UIViewController {
     //Toggle keyboard for billField when the app is launched
     self.billField.becomeFirstResponder()
     print("view DidLoad")
+    //TODO: Get locale currency and separator
+    let locale = Locale.current
+    currencySymbol = locale.currencySymbol ?? "$" //default is $
+    
+    if let currentcySymbol = locale.currencySymbol {
+      print("currency ", currentcySymbol)
+    }
+    if let separator = locale.decimalSeparator {
+      print("separator ", separator)
+    }
     //if app restart, load the data from previous calculation
     loadData()
   }
-
+  
+  func showThousandSeparator(value : Double) -> String {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .decimal
+    numberFormatter.usesGroupingSeparator = true
+    numberFormatter.groupingSeparator = Locale.current.groupingSeparator ?? "."
+    numberFormatter.groupingSize = 3
+    return numberFormatter.string(for: value) ?? "0"
+  }
+  
+  func setAmount(label: UILabel,value : Double = 0) {
+    let rounded_value = round(value * 100) / 100 //Round to 2 decimals
+    let value_str = showThousandSeparator(value: rounded_value)
+    label.text = "\(currencySymbol)\(value_str)"
+  }
+  
   func loadData() {
     let defaults = UserDefaults.init()
     let savedStartTime = defaults.double(forKey: "start_time")
@@ -35,17 +61,31 @@ class ViewController: UIViewController {
       return
     }
     //load data
-    let savedBill = defaults.float(forKey: "bill")
-    let savedTip = defaults.float(forKey: "tip")
-    let savedTotal = defaults.float(forKey: "total")
+    let savedBill = defaults.double(forKey: "bill")
+    let savedTip = defaults.double(forKey: "tip")
+    let savedTotal = defaults.double(forKey: "total")
     let savedTipPercent = defaults.integer(forKey: "tip_percent")
     if savedBill != 0 {
       billField.text = String(format: "%.2f", savedBill)
-      tipLabel.text = String(format: "$%.2f", savedTip)
-      totalLabel.text = String(format: "$%.2f", savedTotal)
+      setAmount(label: tipLabel, value: savedTip)
+      setAmount(label: totalLabel, value: savedTotal)
       tipControl.selectedSegmentIndex = savedTipPercent
       print("time passed: ", NSDate().timeIntervalSince1970 - savedStartTime)
+    } else {
+      setAmount(label: tipLabel)
+      setAmount(label: totalLabel)
     }
+  }
+  
+  /*
+   **Input: a decimal number represents in string which use "comma" as decimal separator
+   **Ouput: a decimal number string use "dot" as decimal separator
+   */
+  func replaceCommaWithDot(str : String) -> String {
+    let range = NSRange(location: 0, length: str.utf16.count)
+    let regex = try! NSRegularExpression(pattern: ",")
+    let new_str = regex.stringByReplacingMatches(in: str, options: [], range: range, withTemplate: ".")
+    return new_str
   }
   
   @IBAction func onTap(_ sender: Any) {
@@ -56,15 +96,16 @@ class ViewController: UIViewController {
   
   @IBAction func calculateTip(_ sender: Any) {
     //get the bill amount
-    let bill = Double(billField.text!) ?? 0
+    let formatted_bill = replaceCommaWithDot(str: billField.text ?? "0")
+    let bill = Double(formatted_bill) ?? 0
     //calculate the tip and total
     let tipPercentages = [0.15, 0.18, 0.2]
     
     let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
     let total = bill + tip
     //update the tip and total labels
-    tipLabel.text = String(format: "$%.2f", tip)
-    totalLabel.text = String(format: "$%.2f", total)
+    setAmount(label: tipLabel, value: tip)
+    setAmount(label: totalLabel, value: total)
     
     //Save data to load when app restart
     let defaults = UserDefaults.init()
